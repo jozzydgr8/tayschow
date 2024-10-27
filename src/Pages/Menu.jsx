f4a6d3420abb6e2ab8704bd1a22280e8c8db77ef
jozzydgr8 <jozzydgreat1@gmail.com>
2024-10-27 14:14:03 -0700

    added menu split

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,34 +1,30 @@
+import { useState } from 'react';
 import { Product } from '../components/Product';
 import { Data } from '../Data';
 export const Menu = ()=>{
+    const [active, setActive] = useState('pastries')
     return(
-
+        <>
         <section id='menu'>
-            <div className="container-fluid">
-                <h1>Our Menu</h1><br/>
-                <div style={{display:"flex"}}>
-
+        <h1 style={{textAlign:'center'}}>Our Menu</h1><br/>
+                <div className='container' style={{display:"flex", alignItems:'center', justifyContent:"space-evenly"}}>
+                    <h4 onClick={()=>setActive('pastries')} className={active == 'pastries'?'menu-active':''}>Pastries</h4>
+                    <h4 onClick={()=>setActive('drink')} className={active == 'drink'?'menu-active':''}>Drinks</h4>
+                    <h4 onClick={()=>setActive('special')} className={active == 'special'?'menu-active':''}>Deals</h4>
                 </div>
-                <h4>Pastries</h4>
+            <div className="container-fluid">
+
                 {
-                    Data && Data.filter(menu => menu.category == 'pastries').map(menu=>(
-                        <Product key={menu.id} menu={menu}/>
+                    Data && Data.filter(menu => menu.category == active).map(menu=>(
+                        <Product key={menu.id} menu={menu} />
                     ))
                 }
-                <h4>Drinks</h4>
-                {
-                    Data && Data.filter(menu => menu.category == 'drink').map(menu=>(
-                        <Product key={menu.id} menu={menu}/>
-                    ))
-                }
-                <h4>Deals</h4>
-                {
-                    Data && Data.filter(menu => menu.category == 'special').map(menu=>(
-                        <Product key={menu.id} menu={menu}/>
-                    ))
-                }
+                <br/>
+                
             </div>
         </section>
+        </>
+        
         
     )
 }

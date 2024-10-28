6ab2fbd38e631573591b495463d21441ff37bcdf
jozzydgr8 <jozzydgreat1@gmail.com>
2024-10-28 09:41:18 -0700

    signwithpopup

--- src/Pages/SignUser.jsx
+++ src/Pages/SignUser.jsx
@@ -1,34 +1,38 @@
-import { signInWithPopup, signInWithRedirect } from "firebase/auth";
+import { signInWithPopup, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { addDoc } from "firebase/firestore";
 import { auth, provider } from "../App";
 import { UseContextAuth } from "../context/UseContextAuth";
 import { userRef } from "../App";
 import {Input, Form} from 'antd'
+import { useEffect } from "react";
+
 
 export const SignUser = ()=>{
 const {dispatch} = UseContextAuth();
-    const handleClick = ()=>{
-        signInWithRedirect(auth, provider).then(data=>{
-            const {user} =data;
-            dispatch({type:'signUser', payload:user});
-            addDoc(userRef, {
+
+    const handleClick = async ()=>{
+        try{
+             const result = await signInWithPopup(auth, provider);
+             const {user } = result.user;
+             dispatch({type:'signUser', payload:user});
+             await addDoc(userRef,{
                 email:user.email,
                 userID:user.uid,
                 phone:user.phoneNumber,
                 name:user.displayName,
-
-            })
-        }).catch(error=>{
+             })
+        }catch(error){
             console.error(error)
-        })
+        }
     }
+        
     return(
         <section style={{marginTop:'80px'}}>
             <div className="container-fluid">
-                <Form>
+                {/* <Form>
                     <Input />
                     <Input.Password/>
-                </Form>
+                </Form> */}
                 <button onClick={handleClick} style={{marginTop:'100px'}}>sign in with gmail</button>
             </div>
         </section>

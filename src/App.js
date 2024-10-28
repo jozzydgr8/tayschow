555ebb9b77454c12c5473222668440ccf9eb7de1
jozzydgr8 <jozzydgreat1@gmail.com>
2024-10-28 03:35:11 -0700

    adding auth

--- src/App.js
+++ src/App.js
@@ -2,8 +2,44 @@ import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } 
 import { Layout } from './Layout';
 import { Index } from './Pages/Index';
 import { useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+import { getAuth } from 'firebase/auth';
 
+
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//appcheeck app cjeck
+window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.REACT_APP_appcheck),
+  isTokenAutoRefreshEnabled: true // This automatically refreshes App Check tokens
+});
+export const auth = getAuth();
 function App() {
+
+  //auth
+
+
+  //animation
   useEffect(()=>{
     const animation = ()=>{
       var containers = document.querySelectorAll('.aSection');
